d7b04db7efeab00c062260c5b202a47db0512a7b
code-master5 <lauhny.bimalk@gmail.com>
2019-01-17 11:20:47 +0530

    Start First Server

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var redis = require('redis');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const PORT = 3000;
+app.listen(PORT);
+console.log(`Server started on port ${PORT}...`)
+
+module.exports = app;
