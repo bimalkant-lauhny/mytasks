6bd8438523c1b9405f8cda33331d20212b2bf174
code-master5 <lauhny.bimalk@gmail.com>
2019-01-18 12:38:50 +0530

    Add tasks

--- app.js
+++ app.js
@@ -6,19 +6,39 @@ const redis = require('redis');
 
 const app = express();
 
+var client = redis.createClient();
+client.on('connect', () => {
+    console.log('Redis Server Connected ...');
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
+app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    client.lrange('tasks', 0, -1, (err, reply) => {
+        res.render('index', {
+            title: 'Task List',
+	    tasks: reply
+    	});
+    });
+});
 
+app.post('/task/add', (req, res) => {
+    var task = req.body.task;
+    client.rpush('tasks', task, (err, reply) => {
+        if (err) {
+	    console.log(err);
+	}
+	console.log('Task Added ...');
+	res.redirect('/');
+    });
+});
 const PORT = 3000;
 app.listen(PORT);
 console.log(`Server started on port ${PORT}...`)
